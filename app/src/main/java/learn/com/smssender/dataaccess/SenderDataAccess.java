bc88351019a1d8168d82c37e7a55075837d0774d
umarfarisi <pharese@gmail.com>
2017-05-06 10:51:41 +0700

    add AsyncTask for get all sender

--- app/src/main/java/learn/com/smssender/dataaccess/SenderDataAccess.java
+++ app/src/main/java/learn/com/smssender/dataaccess/SenderDataAccess.java
@@ -1,10 +1,9 @@
 package learn.com.smssender.dataaccess;
 
-import android.content.ContentProvider;
 import android.content.ContentResolver;
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.ContactsContract;
 
 import java.util.ArrayList;
@@ -21,41 +20,59 @@ import learn.com.smssender.utils.GlobalVariable;
  * @created 05/05/2017
  */
 public class SenderDataAccess {
-    public void getAllSenders(SenderDACallBack<List<Sender>> getAllSendersCallback){
+    public void getAllSenders(final SenderDACallBack<List<Sender>> getAllSendersCallback){
 
-        ContentResolver connResolver = GlobalVariable.APPLICATION_CONTEXT.getContentResolver();
+        new AsyncTask<Void,Void,List<Sender>>(){
 
-        Uri smsUri = Uri.parse("content://sms/inbox");
-        Cursor smsCursor = connResolver.query(smsUri,new String[]{"_id","address"},null,null,null);
+            private ContentResolver resolver;
 
-        Map<String, Integer> smsData = new HashMap<>();
-
-        while(smsCursor.moveToNext()){
-            String senderName = smsCursor.getString(1);
-            if(smsData.containsKey(senderName)){
-                smsData.put(senderName,1+smsData.get(senderName));
-            }else {
-                smsData.put(senderName, 1);
-            }
-        }
-
-        Uri contactUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-
-        List<Sender> data = new ArrayList<>();
-
-        for(Map.Entry<String, Integer> smsDatum: smsData.entrySet()){
-            Cursor contactCursor = connResolver.query(contactUri
-                    ,new String[]{ContactsContract.Contacts.DISPLAY_NAME}
-                    ,ContactsContract.CommonDataKinds.Phone.NUMBER +" = ?"
-                    ,new String[]{smsDatum.getKey()},null);
-            if(contactCursor.moveToNext()){
-                data.add(new Sender(contactCursor.getString(0),smsDatum.getKey(),smsDatum.getValue()));
-            }else{
-                data.add(new Sender(smsDatum.getKey(),smsDatum.getKey(),smsDatum.getValue()));
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                resolver = GlobalVariable.APPLICATION_CONTEXT.getContentResolver();
             }
 
-        }
+            @Override
+            protected List<Sender> doInBackground(Void... params) {
 
-        getAllSendersCallback.onSuccess(data);
+                Uri smsUri = Uri.parse("content://sms/inbox");
+                Cursor smsCursor = resolver.query(smsUri,new String[]{"_id","address"},null,null,null);
+
+                Map<String, Integer> smsData = new HashMap<>();
+
+                while(smsCursor.moveToNext()){
+                    String senderName = smsCursor.getString(1);
+                    if(smsData.containsKey(senderName)){
+                        smsData.put(senderName,1+smsData.get(senderName));
+                    }else {
+                        smsData.put(senderName, 1);
+                    }
+                }
+
+                Uri contactUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+
+                List<Sender> senders = new ArrayList<>();
+
+                for(Map.Entry<String, Integer> smsDatum: smsData.entrySet()){
+                    Cursor contactCursor = resolver.query(contactUri
+                            ,new String[]{ContactsContract.Contacts.DISPLAY_NAME}
+                            ,ContactsContract.CommonDataKinds.Phone.NUMBER +" = ?"
+                            ,new String[]{smsDatum.getKey()},null);
+                    if(contactCursor.moveToNext()){
+                        senders.add(new Sender(contactCursor.getString(0),smsDatum.getKey(),smsDatum.getValue()));
+                    }else{
+                        senders.add(new Sender(smsDatum.getKey(),smsDatum.getKey(),smsDatum.getValue()));
+                    }
+
+                }
+                return senders;
+            }
+
+            @Override
+            protected void onPostExecute(List<Sender> senders) {
+                getAllSendersCallback.onSuccess(senders);
+            }
+
+        }.execute();
     }
 }
